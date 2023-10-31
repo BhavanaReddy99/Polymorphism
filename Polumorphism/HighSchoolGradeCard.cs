357f01eb0f04b63f0f3cae28f0cbb8fecf31e0d3
BhavanaReddy99 <jorrigalabhavana01@gmail.com>
2023-10-31 12:09:01 +0530

    Add project files.

---
+++ Polumorphism/HighSchoolGradeCard.cs
@@ -0,0 +1,21 @@
+﻿using Polymorphism;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class HighSchoolGradeCard : BasicGradeCard
+    {
+        public string HighSchoolInfo { get; set; }
+
+        public override void DisplayGradeCard()
+        {
+            base.DisplayGradeCard();
+            Console.WriteLine($"High School Info: {HighSchoolInfo}");
+        }
+    }
+}
+    
